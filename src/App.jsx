aaf002f0786856afa904c7c97015a250520e827d
Tathila Gomes <112871822+Tathil@users.noreply.github.com>
2022-09-05 12:48:26 +0000

    primeira versão

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import './App.css'
+
+   
+                control={<Switch onChange={this.setMaior} />}
+                label="Maior de idade"
+              />
+            </FormGroup>
+            <TextField
+              onChange={this.setPeso}
+              margin="normal"
+              required
+              fullWidth
+              id="peso"
+              label="Peso"
+              name="peso"
+              type="number"
+              InputProps={{
+                startAdornment:
+                  <InputAdornment position="start">kg</InputAdornment>,
+              }}
+            />
+            <TextField
+              onChange={this.setAltura}
+              margin="normal"
+              required
+              fullWidth
+              id="altura"
+              label="Altura"
+              name="altura"
+              type="number"
+              InputProps={{
+                startAdornment:
+                  <InputAdornment position="start">m</InputAdornment>,
+              }}
+            />
+            <Typography component="h1" variant="h6">
+              {this.resultado()}
+            </Typography>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    );
+  }
+}
 
 export default function App() {
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
-  )
+    <Imc />
+  );
 }
